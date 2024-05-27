d673c5639f859b04301da59c02945cc8d132eae9
adarsh <adarshjai29@gmail.com>
2024-05-27 20:41:16 +0530

    finish country page

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
  const countryDiv = document.querySelector('.main-content')
 // console.log(countryDiv)
 
+
+
 fetch('https://restcountries.com/v3.1/all')
 .then(data=>{
 return data.json() 
@@ -10,24 +12,22 @@ return data.json()
   // debugger
   res.forEach(info => {
     const card = document.createElement('a'); 
-   card.setAttribute('href','#')  // abhi href attribute set karna hai
-    card.classList.add('card');
     // console.log(info)
     card.innerHTML = `<div class="country-image">
     <img src=${info.flags.svg} alt=""> </div>
     <div class="country-info">
     <h3 class="country-name">&nbsp;&nbsp;&nbsp;&nbsp;${info.name.common}</h3><br>
-    <p>&nbsp;&nbsp;&nbsp;&nbsp;<b>Population</b>: ${info.population.toLocaleString()} </p>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;<b>Population</b>: ${info.population.toLocaleString(`en-IN`)} </p>
     <p>&nbsp;&nbsp;&nbsp;&nbsp;<b>Region</b>: ${info.region}</p>
     <p>&nbsp;&nbsp;&nbsp;&nbsp;<b>Capital</b>: ${info.capital} </p>
     </div>`
     
-
+    card.setAttribute('href',`country.html?${info.name.common}`)  
+    card.classList.add('card');
     countryDiv.append(card);
   });
 
-
-
-
- 
 })
+// country detailed part
+
+
